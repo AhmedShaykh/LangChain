c6b20123431c9cef4d64d6b5e33e1715925f119d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-01-25 17:56:00 +0500

    Initial Commit

---
+++ app.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    bool public value = true;
+    int public a = 10;
+    string public myString = "Hello World";
+}
