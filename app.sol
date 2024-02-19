b745a076d24df1781ce30f36ab862f3e22e85e90
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-02-19 15:18:56 +0500

    Delete

--- app.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract MyContract {
-    bool public value = true;
-    int public a = 10;
-    string public myString = "Hello World";
-}
