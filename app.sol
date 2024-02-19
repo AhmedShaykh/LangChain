5438f5c0a959380e1aa83afd8a33bcd475ea4b0a
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-02-19 17:26:34 +0500

    Initial Commit

---
+++ app.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+ 
+pragma solidity ^0.8.24;
+
+contract HelloWorld {
+
+    string greet = "Hello World!";
+
+    uint num = 456; // UInt Not Support Negative Value
+
+    int num2 = -456; // But Int Support Negative Value
+
+    bool isNumber = true;
+
+    address myAccount = 0xb3590E61A1BB15F342d8148d2870d894c4C1FBCF;
+
+}
