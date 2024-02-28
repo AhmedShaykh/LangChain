466785d2554416d87d4d0ab101cb5d586dd2a2e9
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-02-28 18:07:41 +0500

    Add Data Types

--- app.sol
+++ app.sol
@@ -4,14 +4,11 @@ pragma solidity ^0.8.24;
 
 contract HelloWorld {
 
+    // =============== Data Types =============== //
     string greet = "Hello World!";
-
     uint num = 456; // Unsigned Integer Not Support Negative Value
-
     int num2 = -456; // But Integer Support Both Values
-
     bool isNumber = true;
-
     address myAccount = 0xb3590E61A1BB15F342d8148d2870d894c4C1FBCF;
-
+    
 }
