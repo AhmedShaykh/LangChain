fca5f32f0a7d971c647440db23cd9d5250f7dd03
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-03-03 21:27:56 +0500

    Add Access Identifiers

--- app.sol
+++ app.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.24;
 contract HelloWorld {
 
     // =============== Data Types =============== //
-    string greet = "Hello World!";
-    uint num = 456; // Unsigned Integer Not Support Negative Value
-    int num2 = -456; // But Integer Support Both Values
-    bool isNumber = true;
+    string public greet = "Hello World!"; // By Default Varaible Is Private
+    uint public num = 456; // Unsigned Integer Not Support Negative Value
+    int public num2 = -456; // But Integer Support Both Values
+    bool internal  isNumber = true;
     address myAccount = 0xb3590E61A1BB15F342d8148d2870d894c4C1FBCF;
     
 }
