4f17a0d23dafb5433a2a8a43951b6e0c07637578
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-03-25 16:20:06 +0500

    Add Mapping

--- MyMapping.sol
+++ MyMapping.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.24;
 
 contract MyMapping {
 
-    mapping(address => string) public userAddress;
+    mapping(address => string) public userAddress; // Mapping Is Default Stored In Storage
 
-    function addUser(address yourAddress, string memory yourName) public {
-        userAddress[yourAddress] = yourName;
+    function addUser(address _yourAddress, string memory _yourName) public {
+        userAddress[_yourAddress] = _yourName;
     }
+
+    // Array Is Sequential Order (Index) & Mapping Is Non-Sequential Order Direct Access To Particular Value
 
 }
