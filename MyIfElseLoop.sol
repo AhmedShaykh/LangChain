722a01ece5d58e69e1c607c7e04718450b865981
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-04-24 16:41:07 +0500

    Add Loops

--- MyIfElseLoop.sol
+++ MyIfElseLoop.sol
@@ -9,16 +9,50 @@ contract MyIfElse {
 
         if (val > 0) {
             value = "Value Is Greater Than Zero";
-        } 
-        else if (val == 0) {
+        } else if (val == 0) {
             value = "Value Is Equal To Zero";
-        } 
-        else {
+        } else {
             value = "Value Is Less Than Zero";
         }
 
         return value;
 
+    }
+
+}
+
+contract MyLoops {
+
+    uint256[3] public arr;
+
+    uint256 public count;
+
+    // =============== While Loop =============== //
+
+    function whileLoop() public {
+        while (count < arr.length) {
+            arr[count] = count;
+            count++;
+        }
+    }
+
+    // =============== For Loop =============== //
+
+    function forLoop() public {
+        for(uint i = count; i < arr.length; i++) {
+            arr[count] = count;
+            count++;
+        }
+    }
+
+    // =============== While Loop =============== //
+
+    function doWhileLoop() public {
+        do {
+            arr[count] = count;
+            count++;
+        }
+        while(count < arr.length);
     }
 
 }
