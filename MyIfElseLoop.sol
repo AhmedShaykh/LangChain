6afc49981ea963ff387e89dccc342740483369dd
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-04-24 16:18:16 +0500

    Add If Else

--- MyIfElseLoop.sol
+++ MyIfElseLoop.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract MyIfElseLoop {}
+contract MyIfElse {
+
+    function checkValue(int256 val) public pure returns (string memory) {
+
+        string memory value;
+
+        if (val > 0) {
+            value = "Value Is Greater Than Zero";
+        } 
+        else if (val == 0) {
+            value = "Value Is Equal To Zero";
+        } 
+        else {
+            value = "Value Is Less Than Zero";
+        }
+
+        return value;
+
+    }
+
+}
